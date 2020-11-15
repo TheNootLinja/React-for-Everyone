4846d279240d71c1345079d3f1b4dc441efb39f0
TheNootLinja <thenootlinja@gmail.com>
2020-11-14 23:44:37 -0500

    Add initial css for application and get pictures displaying for each movie.

--- src/movies/Movie.js
+++ src/movies/Movie.js
@@ -1,14 +1,20 @@
 import PropTypes from 'prop-types';
 
-export function Movie({ movie }) {
+export function Movie({ movie, config }) {
 
     return (
-        <li key={movie.id}>{movie.title}</li>
+        <li key={movie.id}>
+            {config.images?.base_url && 
+            <img src={config.images.base_url + "w342" + movie.poster_path} alt={movie.title + " Poster"}/>
+            }
+            <h3>{movie.title}</h3>
+        </li>
     )
 }
 
 Movie.propTypes = {
     movie: PropTypes.shape({
         title: PropTypes.string.isRequired,
+        poster_path: PropTypes.string.isRequired,
     }).isRequired,
 }
--- src/movies/MoviesList.js
+++ src/movies/MoviesList.js
@@ -2,11 +2,15 @@ import { useState, useEffect } from 'react';
 import { Movie } from './Movie.js';
 import { Filter } from '../Filter.js';
 
-const API_URL = "https://api.themoviedb.org/3/discover/movie?api_key=bd5d8ee62a1a0d97362d3c5924713f9a&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
+const API_URL = 
+    "https://api.themoviedb.org/3/discover/movie?api_key=bd5d8ee62a1a0d97362d3c5924713f9a&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
+const CONFIG_URL = 
+    "https://api.themoviedb.org/3/configuration?api_key=bd5d8ee62a1a0d97362d3c5924713f9a";
 
 export function MoviesList() {
     const [filter, setFilter] = useState("");
     const [movies, setMovies] = useState([]);
+    const [config, setConfig ] = useState({});
 
     const fetchMovies = async () => {
         try {
@@ -18,21 +22,31 @@ export function MoviesList() {
             console.error(e)
         }
     }
+    const fetchConfig = async () => {
+        try {
+            const res = await fetch(CONFIG_URL);
+            const config = await res.json();
+            setConfig(config);
+        } catch (e) {
+            console.error(e)
+        }
+    }
 
     useEffect( () => {
+        fetchConfig();
         fetchMovies();
     }, [])
 
     return (
         <div>
             <Filter filter={filter} setFilter={setFilter}/>
-            <ul>
+            <ul className="movies-list">
                 { movies.filter(movie => 
                 movie.title.toLowerCase()
                 .includes(filter.toLowerCase()))
                 .map(( movie ) =>
                     (
-                        <Movie movie={movie} key={movie.id}/>
+                        <Movie movie={movie} config={config} key={movie.id}/>
                     )
                 )}
             </ul>
